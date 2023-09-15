0c22638db6d7892c670e7f4f4efe859174d565c6
wknishio@gmail.com <wknishio@gmail.com>
2023-09-15 14:55:45 -0300

    removed groovy and windows utilities

--- util/groovyc.bat
+++
@@ -1,2 +0,0 @@
-cd groovy-2.2.2/script
-groovyc.bat
--- util/windows/uac/disable-uac-admin-users.bat
+++
@@ -1,1 +0,0 @@
-%windir%\System32\cmd.exe /k %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
