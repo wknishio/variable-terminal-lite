440cc40e6a4b7b69ce17628338a7a505a5425a96
wknishio@gmail.com <wknishio@gmail.com>
2023-09-26 11:13:22 -0300

    implemented delay when cannot listen for connection

--- src/vate/org/vash/vate/client/connection/VTClientConnector.java
+++ src/vate/org/vash/vate/client/connection/VTClientConnector.java
@@ -498,11 +498,15 @@ public class VTClientConnector implements Runnable
     }
     catch (Throwable e)
     {
-      // VTTerminal.print("VT>TCP port [" + port + "] is already in
-      // use!\n");
-      // e.printStackTrace();
       VTConsole.print("\nVT>Listening to connection in port [" + port + "] failed!");
-      // return false;
+    }
+    try
+    {
+      Thread.sleep(250);
+    }
+    catch (Throwable t)
+    {
+      
     }
     return false;
   }
