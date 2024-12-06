a408a0fd0c356ca49723cfc29b2b11a4a22fdbbb
wknishio@gmail.com <wknishio@gmail.com>
2024-12-06 13:03:57 -0300

    fixed non-graphical console

--- src/vate/org/vash/vate/server/VTServer.java
+++ src/vate/org/vash/vate/server/VTServer.java
@@ -76,7 +76,7 @@ public class VTServer implements Runnable
   private int pingLimit = 0;
   private int pingInterval = 0;
   private int reconnectTimeout = 0;
-  private Future<?> startThread;
+  private Future<?> runThread;
   
   private static final String VT_SERVER_SETTINGS_COMMENTS = 
   "Variable-Terminal server settings file, supports UTF-8\r\n" + 
@@ -2234,16 +2234,35 @@ public class VTServer implements Runnable
   
   public void start()
   {
-    startThread = executorService.submit(new Runnable()
+    runThread = executorService.submit(new Runnable()
     {
       public void run()
       {
-        startServer();
+        runServer();
+      }
+    });
+    try
+    {
+      runThread.get();
+    }
+    catch (Throwable t)
+    {
+      
+    }
+  }
+  
+  public void startThread()
+  {
+    runThread = executorService.submit(new Runnable()
+    {
+      public void run()
+      {
+        runServer();
       }
     });
   }
   
-  private void startServer()
+  private void runServer()
   {
     Thread.setDefaultUncaughtExceptionHandler(new VTUncaughtExceptionHandler());
     // loadFileServerSettings();
@@ -2484,9 +2503,9 @@ public class VTServer implements Runnable
   
   public boolean isRunning()
   {
-    if (startThread != null)
+    if (runThread != null)
     {
-      return !startThread.isDone();
+      return !runThread.isDone();
     }
     return false;
   }
