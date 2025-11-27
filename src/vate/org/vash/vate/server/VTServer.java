c10c26e746f837d0d6bea0086337601c2e6050c0
wknishio@gmail.com <wknishio@gmail.com>
2025-11-27 13:04:21 -0300

    updated managed socket api

--- src/vate/org/vash/vate/server/VTServer.java
+++ src/vate/org/vash/vate/server/VTServer.java
@@ -81,7 +81,7 @@ public class VTServer implements Runnable
   private int reconnectTimeout = 0;
   private Future<?> runThread;
   private VTDataMonitorService monitorService;
-  private VTProxy[] proxies = new VTProxy[] {};
+  private VTProxy proxy;
   
   private static final String VT_SERVER_SETTINGS_COMMENTS = 
   "Variable-Terminal server settings file, supports UTF-8\r\n" + 
@@ -95,9 +95,31 @@ public class VTServer implements Runnable
     VTSystem.initialize();
   }
   
-  public VTServer(VTProxy... proxies)
+  public VTServer()
   {
-    this.proxies = proxies;
+    this.executorService = Executors.newCachedThreadPool(new ThreadFactory()
+    {
+      public Thread newThread(Runnable runnable)
+      {
+        Thread created = new Thread(null, runnable, runnable.getClass().getSimpleName());
+        created.setDaemon(true);
+        created.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+        return created;
+      }
+    });
+    // this.audioSystem = new VTAudioSystem[5];
+    // this.audioSystem[0] = new VTAudioSystem(executor);
+    // this.audioSystem[1] = new VTAudioSystem(executor);
+    // this.audioSystem[2] = new VTAudioSystem(executor);
+    // this.audioSystem[3] = new VTAudioSystem(executor);
+    // this.audioSystem[4] = new VTAudioSystem(executor);
+    
+    // loadServerSettingsFile();
+  }
+  
+  public VTServer(VTProxy proxy)
+  {
+    this.proxy = proxy;
     this.executorService = Executors.newCachedThreadPool(new ThreadFactory()
     {
       public Thread newThread(Runnable runnable)
@@ -2340,9 +2362,9 @@ public class VTServer implements Runnable
     }
   }
   
-  public void setProxies(VTProxy... proxies)
+  public void setProxy(VTProxy proxy)
   {
-    this.proxies = proxies;
+    this.proxy = proxy;
   }
   
   public void run()
@@ -2351,7 +2373,7 @@ public class VTServer implements Runnable
     {
       executorService.execute(monitorService);
     }
-    serverConnector = new VTServerConnector(this, new VTBlake3SecureRandom(), proxies);
+    serverConnector = new VTServerConnector(this, new VTBlake3SecureRandom(), proxy);
     serverConnector.setPassive(passive);
     serverConnector.setAddress(hostAddress);
     serverConnector.setPort(hostPort);
