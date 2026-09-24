21c7d59bec7916947fa9f0399785a63f0028cc60
wknishio <wknishio@gmail.com>
2026-09-24 13:09:32 -0300

    restored support for GRAIN encryption

--- src/vate/org/vash/vate/server/VTServer.java
+++ src/vate/org/vash/vate/server/VTServer.java
@@ -92,7 +92,7 @@ public class VTServer implements Runnable
   "#vate.server.connection.mode      values: default passive(P), active(A)\r\n" + 
   "#vate.server.proxy.type           values: DIRECT(D)/SOCKS(S)/HTTP(H)/PLUS(P)\r\n" + 
   "#vate.server.authentication.type  values: DEFAULT(D) or TLS(T)\r\n" + 
-  "#vate.server.encryption.type      values: SALSA(S)/HC(H)/ZUC(Z)/LEA(L)\r\n" + 
+  "#vate.server.encryption.type      values: SALSA(S)/HC(H)/GRAIN(G)/ZUC(Z)/LEA(L)\r\n" + 
   "#vate.server.session.accounts     format: user1/password1;user2/password2;...\r\n";
   
   static
@@ -1861,7 +1861,7 @@ public class VTServer implements Runnable
         }
         if (line.toUpperCase().startsWith("Y"))
         {
-          VTMainConsole.print("VT>Enter encryption type(SALSA(S)/HC(H)/ZUC(Z)/LEA(L)):");
+          VTMainConsole.print("VT>Enter encryption type(SALSA(S)/HC(H)/GRAIN(G)/ZUC(Z)/LEA(L)):");
           line = VTMainConsole.readLine(false);
           if (line == null)
           {
@@ -1876,10 +1876,10 @@ public class VTServer implements Runnable
           {
             encryptionType = "HC";
           }
-//          if (line.toUpperCase().startsWith("G"))
-//          {
-//            encryptionType = "GRAIN";
-//          }
+          if (line.toUpperCase().startsWith("G"))
+          {
+            encryptionType = "GRAIN";
+          }
 //          if (line.toUpperCase().startsWith("R"))
 //          {
 //            encryptionType = "RABBIT";
